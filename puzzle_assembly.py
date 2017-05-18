fa30e63a63c92d1c369a4c31bac03d25a099d0c2
connermcb <connermcb@yahoo.com>
2017-05-17 20:47:09 -0500

    Add top_bottom function

--- puzzle_assembly.py
+++ puzzle_assembly.py
@@ -26,25 +26,50 @@ class Puzzle(object):
             self.edges[edge_key].append(line)
         else:
             self.middle.append(line)
-        
         return
         
-p = Puzzle(3)
+    def top_bottom(self):
+        top_perms = It.permutations(self.edges[0])
+        for each in top_perms:
+            test = True
+            for i in range(len(each)-1):
+                if each[i][3] != each[i+1][1]:
+                    test = False
+            if test and each[0][1] == self.results[0][0][3] and each[-1][3]==self.results[0][self.dims-1][1]:
+                self.results[0][1:-1] = each
+                break
+        bottom_perms = It.permutations(self.edges[2])
+        for each in bottom_perms:
+            test = True
+            for i in range(len(each)-1):
+                if each[i][3] != each[i+1][1]:
+                    test = False
+            if test and each[0][1] == self.results[self.dims-1][0][3] and each[-1][3]==self.results[self.dims-1][self.dims-1][1]:
+                self.results[self.dims-1][1:-1] = each
+        return
+                
+        
+        
+p = Puzzle(4)
 
+l= [('black','black','green','red'),('black','red','blue','green'),('black','green','red','yellow'),('black','yellow','blue','black'),
+    ('green','black','blue','purple'),('blue','purple','purple','green'),('red','green','blue','yellow'),('blue','yellow','green','black'),
+    ('blue','black','yellow','green'),('purple','green','red','yellow'),('blue','yellow','green','purple'),('green','purple','yellow','black'),
+    ('yellow','black','black','purple'),('red','purple','black','purple'),('green','purple','black','blue'),('yellow','blue','black','black'),]
 
-l = [('yellow','black','black','blue'), 
-     ('blue','blue','black','yellow'), 
-     ('orange','yellow','black','black'),
-     ('red','black','yellow','green'),
-     ('orange','green','blue','blue'),
-     ('green','blue','orange','black'),
-     ('black','black','red','red'),
-     ('black','red','orange','purple'),
-     ('black','purple','green','black')]
+#l = [('yellow','black','black','blue'), 
+#     ('blue','blue','black','yellow'), 
+#     ('orange','yellow','black','black'),
+#     ('red','black','yellow','green'),
+#     ('orange','green','blue','blue'),
+#     ('green','blue','orange','black'),
+#     ('black','black','red','red'),
+#     ('black','red','orange','purple'),
+#     ('black','purple','green','black')]
 
 for line in l:
     p.read_data(line)
-print(p.edges)
-
+print(p.top_bottom())
+print(p.results)
         
         
