ea7e95d114d10342a2b0cdb68e1db37c6ba393d6
connermcb <connermcb@yahoo.com>
2017-05-17 19:22:51 -0500

    Create __init__ for Puzzle class

--- puzzle_assembly.py
+++ puzzle_assembly.py
@@ -8,6 +8,13 @@ class Puzzle(object):
     def __init__(self, dims):
         self.dims = dims
         self.results = [[] * dims]
+        self.corners = {(1, 1, 0, 0):(0, 0), (0, 1, 1, 0):(0, 4), (0, 1, 1, 0):(4, 0), (1, 0, 0, 1):(4, 4)}
+        self.top = []
+        self.left = []
+        self.right = []
+        self.bottom = []
         
-
+    def read_data(self, line):
+        
+        
         
