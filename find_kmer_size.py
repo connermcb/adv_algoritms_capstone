9b281e04a23ebe8e821b3833a4c7acac661669fe
connermcb <connermcb@yahoo.com>
2017-05-24 23:32:38 -0500

    Add is_balanced() check node degree balance

--- find_kmer_size.py
+++ find_kmer_size.py
@@ -8,57 +8,90 @@ Optimal k-mer Size
 class Graph(object):
     
     def __init__(self):
+        self.reads = []
         self.node_set = []
+        self.start = None
         self.graph = {}
         self.degrees = {}
         self.components = {}
         
     def add_node(self, node):
-        self.node_set.append(node)
+        self.reads.append(node)
         
     def divide_kmers(self, k):
-        orgnl = len(self.node_set[0])
+        self.node_set = []
+        orgnl = len(self.reads[0])
         if k == orgnl:
+            self.node_set = list(self.reads)
             return
-        temp = []
-        for node in self.node_set:
+        for node in self.reads:
             for i in range(orgnl - k + 1):
-                temp.append(node[i:i+k])
-        self.node_set = list(temp)
+                self.node_set.append(node[i:i+k])
         
     def make_graph(self):
+        self.graph = {}
+        self.start = self.node_set[0][:-1]
         for node in self.node_set:
             self.components[node[:-1]] = -1
             self.components[node[1:]] = -1
             self.components[self.node_set[0][:-1]] = 1
         for kmer in self.node_set:
-            if kmer in self.graph:
+            if kmer[:-1] in self.graph:
                 self.graph[kmer[:-1]].append(kmer[1:])
-                self.degrees[kmer[:-1]][0] += 1
-                self.degrees[kmer[1:]][1] += 1
             else:
-                self.graph[kmer[:-1]] = kmer[1:]
-                self.degrees[kmer[:-1]] = [1,0]
-                self.degrees[kmer[1:]] = [0,1]
+                self.graph[kmer[:-1]] = [kmer[1:]]
 
-                
-                
-    def is_connected(self):
+
+    def calc_degrees(self):     
+        self.degrees = {}
+        nodes = self.graph.keys()
+        for n in nodes:
+            self.degrees[n] = [0,0]
+        for n1 in nodes:
+            for n2 in nodes:
+                if n1[1:] == n2[:-1]:
+                    self.degrees[n1][0] += 1
+                    self.degrees[n2][1] += 1
         
+        
+    def is_connected(self):
+        queue = [self.start]
+        while queue:
+            current = queue.pop(0)
+            for each in self.graph[current]:
+                self.components[each] = current
+                queue.append(each)
         return True
     
     def is_balanced(self):
+        if not self.degrees:
+            return False
         for each in self.degrees.items():
             if each[1][0] != each[1][1]:
                 return False
         return True
     
     def run(self):
-        pass
+        for k in range(100, 1, -1):
+            self.divide_kmers(i)
+            self.make_graph()
+            self.calc_degrees()
+            if g.is_balanced():
+                print(k)
+                return
     
 g = Graph()
 
-l = ['ATCCGA']
-g.add_node(l[0])
-g.divide_kmers(2)
-print(g.node_set)
+l = ['AACG','ACGT','CAAC','GTTG','TGCA']
+for each in l:
+    g.add_node(each)
+g.divide_kmers(4)
+g.make_graph()
+g.calc_degrees()
+print(g.is_balanced())
+g.divide_kmers(3)
+g.make_graph()
+g.calc_degrees()
+print(g.graph)
+print(g.degrees)
+print(g.is_balanced())
