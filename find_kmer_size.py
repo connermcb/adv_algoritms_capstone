271c2691125ec303fe30aec009aa31c233d16db5
connermcb <connermcb@yahoo.com>
2017-05-24 21:35:31 -0500

    Add divide_kmers()

--- find_kmer_size.py
+++ find_kmer_size.py
@@ -16,14 +16,25 @@ class Graph(object):
     def add_node(self, node):
         self.node_set.append(node)
         
+    def divide_kmers(self, k):
+        orgnl = len(self.node_set[0])
+        if k == orgnl:
+            return
+        temp = []
+        for node in self.node_set:
+            for i in range(orgnl - k + 1):
+                temp.append(node[i:i+1])
+                
         
     def make_graph(self, k):
         for node1 in self.node_set:
             for node2 in self.node_set:
-                
+                pass
                 
     def has_euler(self):
         return True
     
     def run(self):
         pass
+    
+    
