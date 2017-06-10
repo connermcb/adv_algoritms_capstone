d6268844d75b77137a611abc9c56d822a731daf9
connermcb <connermcb@yahoo.com>
2017-06-10 16:36:32 -0500

    Remove assemble_genome()
    Removed method from architecture in favor of starting
    method that combines build graph and assemble genome.

--- error_prone_overlap_assembler.py
+++ error_prone_overlap_assembler.py
@@ -4,6 +4,7 @@
 Overlap Assembler with Error-Prone Reads
 """
 import itertools as it
+from statistics import mode
 import string
 import sys
 
@@ -14,31 +15,50 @@ class OverlapEP(object):
         self.read_length = read_length
         self.reads = []
         self.graph = {}
-        self.result = ""
+        self.result = []
+
+    def add_line(self, line):
+        self.reads.append(line)
+        self.graph[line] = []
 
     def hamming_dist(self, r1, r2):
+#        print(r1, r2)
         dist = 0
         for i in range(len(r1)):
             if r1[i] != r2[i]:
                 dist += 1
             if dist > 2:
-                break
-        return dist
+                return False
+        return True
                 
     def build_graph_ep(self):
-        self.perms = it.permutations(reads, 2)
+        self.perms = it.permutations(self.reads, 2)
         for pair in self.perms:
             r1, r2 = pair
-            range_end = self.graph[r1][1]
+            range_end = self.read_length
             pos = 0
-            for i in range(0, range_end):
+            for i in range(0, range_end-3):
+                if len(self.graph[r1]) == 4:
+                    break
                 if self.hamming_dist(r1[i:], r2[:self.read_length-i]):
                     pos = int(i)
                     break
             if pos > 0:
-                self.graph[r1] = (r2, pos)
+                self.graph[r1].append((r2, pos))
                 
     def assemble_genome(self):
+        nxt = self.reads[0]
+        stack = [each for each in self.graph[nxt]]
+        print(stack)
+#        self.result = [[ltr] for ltr in nxt]
+#        while stack:
+#            if nxt == self.reads[0]:
+#                break
+            
+            
+            
+            
+        print(self.result)
         pass
     
     def trim(self):
@@ -48,5 +68,20 @@ class OverlapEP(object):
                 self.result = self.result[:-pos]
                 return
             pos += 1
-            
+
+reads = ['ABCDEFGHIJ','CDEFGHIJKL','GHIJKLMNOP','MNOPQRSTUV','NOPQRSTUVW','QRSTUVWXYZ','UVWXYZABCD']
+        
+#genome = string.ascii_uppercase
+#genome += genome[:10]
+#reads = [genome[i:i+5] for i in range(len(genome)-5)]
+o = OverlapEP(10)
+for each in reads:
+    o.add_line(each)
+o.build_graph_ep()
+print(o.graph)
+o.assemble_genome()
+
+#o.find_path()
+#o.trim()
+#print(o.result)   
     
