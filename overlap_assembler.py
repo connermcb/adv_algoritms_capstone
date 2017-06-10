b42977bfe0b793d57e1a8cfdf40ec0878781027e
connermcb <connermcb@yahoo.com>
2017-06-09 19:56:16 -0500

    Add break to build_graph() and correct indexing
    Correct indexing alignments across all methods and
    add very important break to build_graph and get
    run time within specifications

--- overlap_assembler.py
+++ overlap_assembler.py
@@ -18,25 +18,27 @@ class Overlap(object):
     
     def add_line(self, line):
         self.reads.append(line)
-        self.graph[line] = []
+        self.graph[line] = (None, self.read_length)
         
     def build_graph(self):
         self.perms = it.permutations(reads, 2)
         for pair in self.perms:
             r1, r2 = pair
+            range_end = self.graph[r1][1]
             pos = 0
-            for i in range(0, self.read_length):
-                if r1[-i:] == r2[:i]:
+            for i in range(0, range_end):
+                if r1[i:] == r2[:self.read_length-i]:
                     pos = int(i)
+                    break
             if pos > 0:
-                self.graph[r1].append((r2, pos))
+                self.graph[r1] = (r2, pos)
 
     def find_path(self):
         nxt = self.reads[0]
         self.result += nxt
-        for i in range(len(self.reads)):
-            nxt, pos = max(self.graph[nxt], key=lambda x:x[1])
-            self.result += nxt[pos:]
+        for i in range(len(self.graph)):
+            nxt, pos = self.graph[nxt]
+            self.result += nxt[self.read_length-pos:]
             
     
     
@@ -45,13 +47,11 @@ class Overlap(object):
 #genome = string.ascii_uppercase
 #genome += genome[:10]
 #reads = [genome[i:i+5] for i in range(len(genome)-5)]
-#print(reads)
 #o = Overlap(5)
 #for each in reads:
 #    o.add_line(each)
 #o.build_graph()
 #o.find_path()
-#print(o.result)
 reads = []
 for i in range(1618):
     reads += [sys.stdin.readline().strip()]
