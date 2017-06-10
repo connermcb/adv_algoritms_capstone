7c78b749bee0f6cd8d5506fb479f6c9a0ca03068
connermcb <connermcb@yahoo.com>
2017-06-09 19:14:47 -0500

    Add build_graph() and find_path()
    Graph builder that finds all pairs with overlap using
    permutations of all reads
    
    First attempt at finding hamiltonian path with
    	greedy algorithm

--- overlap_assembler.py
+++ overlap_assembler.py
@@ -3,6 +3,8 @@
 """
 Overlap Assembler
 """
+import itertools as it
+import string
 
 import sys
 
@@ -18,15 +20,45 @@ class Overlap(object):
         self.reads.append(line)
         self.graph[line] = []
         
+    def build_graph(self):
+        self.perms = it.permutations(reads, 2)
+        for pair in self.perms:
+            r1, r2 = pair
+            pos = 0
+            for i in range(0, self.read_length):
+                if r1[-i:] == r2[:i]:
+                    pos = int(i)
+            if pos > 0:
+                self.graph[r1].append((r2, pos))
+
+    def find_path(self):
+        nxt = self.reads[0]
+        self.result += nxt
+        for i in range(len(self.reads)):
+            nxt, pos = max(self.graph[nxt], key=lambda x:x[1])
+            self.result += nxt[pos:]
+            
     
-
-
-
+    
+        
+    
+#genome = string.ascii_uppercase
+#genome += genome[:10]
+#reads = [genome[i:i+5] for i in range(len(genome)-5)]
+#print(reads)
+#o = Overlap(5)
+#for each in reads:
+#    o.add_line(each)
+#o.build_graph()
+#o.find_path()
+#print(o.result)
 reads = []
 for i in range(1618):
     reads += [sys.stdin.readline().strip()]
 o=Overlap(100)
 for read in reads:
     o.add_line(read)
-o.assemble()
+o.build_graph()
+o.find_path()
 print(o.result)
+
