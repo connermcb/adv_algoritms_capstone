ffa6a22e8bd098017c5cd35fffeadccffdcc3721
connermcb <connermcb@yahoo.com>
2017-06-18 13:57:06 -0500

    Make numpy implements of build_graph, reads_compare
    New implementations of graph building function
    and helper function for comparing reads.

--- error_prone_overlap_assembler.py
+++ error_prone_overlap_assembler.py
@@ -6,31 +6,42 @@ Overlap Assembler with Error-Prone Reads
 import phi_X174_formatter as fmtr
 import itertools as it
 import numpy as np
+import random
 import read_maker
 from statistics import mode
+import string
 import sys
 
 
 class OverlapEP(object):
     
-    def __init__(self, read_length):
+    def __init__(self, read_length, reads):
         self.read_length = read_length
-        self.reads = []
+        self.reads = np.array(reads)
+        self.pair_overlap_list = np.zeros((1618, 2))
+        self.searched = np.zeros(1618, dtype=bool)
         self.graph = {}
         self.result = []
+        self.nxt = 0
 
+    def compare(self, nxt):
+        for i in range(99, 10, -1):
+            test = self.reads[nxt, 100-i:] == self.reads[:, :i]
+            counts = np.count_nonzero(test==0, axis=1)
+            matches = np.nonzero(counts < 3)
+            if matches[0]:
+                self.pair_overlap_list[nxt, :] = [matches[0], i]
+                self.searched[nxt] = True
+                self.nxt = matches[0]
+                return
+            
     def build_graph_ep(self):
-        self.perms = it.permutations(self.reads, 2)
-        for pair in self.perms:
-            r1, r2 = pair
-            range_end = self.graph[r1][1]
-            pos = 0
-            for i in range(0, range_end):
-                if self.hamming_dist(r1[i:], r2[:self.read_length-i]):
-                    pos = int(i)
-                    break
-            if pos > 0:
-                self.graph[r1] = (r2, pos)
+        counter = 0
+        while not np.all(self.searched):
+            if counter > 1618:
+                return
+            counter += 1
+            self.compare(nxt)
 
     def get_cumulative_indices(self):
         self.cumulative_indices = {read:0 for read in self.reads}
@@ -72,15 +83,36 @@ class OverlapEP(object):
             pos += 1
 
 
+test_reads = ["".join([random.choice('ACGT') for i in range(10)])\
+              for j in range(1618)]
 
+test_reads = [list(each) for each in test_reads]
 
+O = OverlapEP(10, test_reads)
+print(O.reads)
+#print(test_reads)
+#print(len(test_reads))
+#print(len(test_reads[0]))
+#
+#reads = np.array(test_reads)#[:, np.newaxis]
+#print(reads)
+#print(reads.shape)
+#print(reads[0])
+#print(reads[1])
+#print(reads[0,0])
+#import time
+#start = time.time()
+#check = reads[0,:]==reads[1:,:]
+#print(time.time()-start)
+#print(check)
+#print(reads[9,0])
+#print(reads[0,0]==reads[1:,0])
 #if __name__ == "__main__":
-#    o = OverlapEP(100)
-#    genome = fmtr.reader()
-#    reads = read_maker.make_reads(genome, 1618)
-#    for r in reads:
-#        o.add_line(r)
-#    o.build_graph_ep()
+#    reads = []
+#    for i in range(1618):
+#        read = sys.stdin.readline().strip()
+#        reads.append(list(read))
+#    O = OverlapEP(100, reads)
 
 
 
